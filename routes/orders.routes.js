2462b0f687b1ae80fb1a10d61bb0e3666540fb57
PRIYANKARANGISETTY <161281072+PRIYANKARANGISETTY@users.noreply.github.com>
2026-01-21 18:50:55 +0530

    Update orders.routes.js

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -23,3 +23,43 @@ router.post("/", (req, res) => {
 
   res.status(201).json(order);
 });
+router.get("/", (req, res) => {
+  res.json(readDB().orders);
+});
+router.delete("/:orderId", (req, res) => {
+  const db = readDB();
+  const order = db.orders.find(o => o.id == req.params.orderId);
+
+  if (!order) return res.status(404).json({ message: "Order not found" });
+  if (order.status === "cancelled")
+    return res.status(400).json({ message: "Already cancelled" });
+
+  const today = new Date().toISOString().split("T")[0];
+  if (order.createdAt !== today)
+    return res.status(400).json({ message: "Cannot cancel today" });
+
+  order.status = "cancelled";
+  const product = db.products.find(p => p.id === order.productId);
+  product.stock += order.quantity;
+
+  writeDB(db);
+  res.json(order);
+});
+router.patch("/change-status/:orderId", (req, res) => {
+  const db = readDB();
+  const order = db.orders.find(o => o.id == req.params.orderId);
+  const { status } = req.body;
+
+  if (!order) return res.status(404).json({ message: "Order not found" });
+  if (["cancelled", "delivered"].includes(order.status))
+    return res.status(400).json({ message: "Status change not allowed" });
+
+  const flow = ["placed", "shipped", "delivered"];
+  if (flow.indexOf(status) !== flow.indexOf(order.status) + 1)
+    return res.status(400).json({ message: "Invalid flow" });
+
+  order.status = status;
+  writeDB(db);
+  res.json(order);
+});
+
